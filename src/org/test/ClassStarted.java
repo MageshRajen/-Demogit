1ff0b25c2250667c5c20a70f3e379a6e658c18c2
MageshRajen <mageshr11@gmail.com>
2022-10-14 18:12:51 +0530

    Java

---
+++ src/org/test/ClassStarted.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class ClassStarted {
+	public void javaCourse() {
+		System.out.println("Javacourse");
+}
+	public void pythonCourse() {
+		System.out.println("python");
+	}
+	public static void main(String[] args) {
+		ClassStarted s = new ClassStarted();
+		s.javaCourse();
+		s.pythonCourse();
+		
+	}
+}
